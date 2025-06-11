0850c6014502a26e8146394bc3ee64e1d56f2d20
Francesco <66080458+chicco-carone@users.noreply.github.com>
2025-06-11 13:32:44 +0000

    Add constants for service

--- custom_components/power_load_balancer/const.py
+++ custom_components/power_load_balancer/const.py
@@ -5,3 +5,9 @@ CONF_POWER_BUDGET_WATT = "power_budget_watt"
 CONF_APPLIANCE = "appliance"
 CONF_IMPORTANCE = "importance"
 CONF_LAST_RESORT = "last_resort"
+
+SERVICE_TURN_OFF_APPLIANCE = "turn_off_appliance"
+SERVICE_TURN_ON_APPLIANCE = "turn_on_appliance"
+
+ATTR_ENTITY_ID = "entity_id"
+ATTR_REASON = "reason"
